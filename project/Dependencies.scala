b47936595eed340fc3b2693d331db1b04a9ce5c9
Bruno Ferreira <bruno.ferreira@codacy.com>
2018-08-08 10:44:34 +0100

    Updates specs2 dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,6 +6,6 @@ object Dependencies {
     val metricsSeed = "com.codacy" %% "codacy-metrics-scala-seed" % "0.1.219"
   }
 
-  val specs2Version = "4.2.0"
+  val specs2Version = "4.3.2"
   val specs2 = "org.specs2" %% "specs2-core" % specs2Version
 }
