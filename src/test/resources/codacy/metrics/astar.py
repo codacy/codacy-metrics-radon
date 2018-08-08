92747e77d053d5f7703c80bcb37d1aff676f6a8b
Bruno Ferreira <bruno.ferreira@codacy.com>
2018-08-08 09:57:54 +0100

    Fix tests

--- src/test/resources/codacy/metrics/astar.py
+++ src/test/resources/codacy/metrics/astar.py
@@ -1,85 +1,179 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-class Node:
-    def __init__(self,value,point):
-        self.value = value
-        self.point = point
-        self.parent = None
-        self.H = 0
-        self.G = 0
-    def move_cost(self,other):
-        return 0 if self.value == '.' else 1
+class SimpleGraph:
+    def __init__(self):
+        self.edges = {}
 
-def children(point,grid):
-    x,y = point.point
-    links = [grid[d[0]][d[1]] for d in [(x-1, y),(x,y - 1),(x,y + 1),(x+1,y)]]
-    return [link for link in links if link.value != '%']
-def manhattan(point,point2):
-    return abs(point.point[0] - point2.point[0]) + abs(point.point[1]-point2.point[0])
-def aStar(start, goal, grid):
-    #The open and closed sets
-    openset = set()
-    closedset = set()
-    #Current point is the starting point
-    current = start
-    #Add the starting point to the open set
-    openset.add(current)
-    #While the open set is not empty
-    while openset:
-        #Find the item in the open set with the lowest G + H score
-        current = min(openset, key=lambda o:o.G + o.H)
-        #If it is the item we want, retrace the path and return it
+    def neighbors(self, id):
+        return self.edges[id]
+
+example_graph = SimpleGraph()
+example_graph.edges = {
+    'A': ['B'],
+    'B': ['A', 'C', 'D'],
+    'C': ['A'],
+    'D': ['E', 'A'],
+    'E': ['B']
+}
+
+import collections
+
+class Queue:
+    def __init__(self):
+        self.elements = collections.deque()
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, x):
+        self.elements.append(x)
+
+    def get(self):
+        return self.elements.popleft()
+
+# utility functions for dealing with square grids
+def from_id_width(id, width):
+    return (id % width, id // width)
+
+def draw_tile(graph, id, style, width):
+    r = "."
+    if 'number' in style and id in style['number']: r = "%d" % style['number'][id]
+    if 'point_to' in style and style['point_to'].get(id, None) is not None:
+        (x1, y1) = id
+        (x2, y2) = style['point_to'][id]
+        if x2 == x1 + 1: r = ">"
+        if x2 == x1 - 1: r = "<"
+        if y2 == y1 + 1: r = "v"
+        if y2 == y1 - 1: r = "^"
+    if 'start' in style and id == style['start']: r = "A"
+    if 'goal' in style and id == style['goal']: r = "Z"
+    if 'path' in style and id in style['path']: r = "@"
+    if id in graph.walls: r = "#" * width
+    return r
+
+def draw_grid(graph, width=2, **style):
+    for y in range(graph.height):
+        for x in range(graph.width):
+            print("%%-%ds" % width % draw_tile(graph, (x, y), style, width), end="")
+        print()
+
+# data from main article
+DIAGRAM1_WALLS = [from_id_width(id, width=30) for id in [21,22,51,52,81,82,93,94,111,112,123,124,133,134,141,142,153,154,163,164,171,172,173,174,175,183,184,193,194,201,202,203,204,205,213,214,223,224,243,244,253,254,273,274,283,284,303,304,313,314,333,334,343,344,373,374,403,404,433,434]]
+
+class SquareGrid:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.walls = []
+
+    def in_bounds(self, id):
+        (x, y) = id
+        return 0 <= x < self.width and 0 <= y < self.height
+
+    def passable(self, id):
+        return id not in self.walls
+
+    def neighbors(self, id):
+        (x, y) = id
+        results = [(x+1, y), (x, y-1), (x-1, y), (x, y+1)]
+        if (x + y) % 2 == 0: results.reverse() # aesthetics
+        results = filter(self.in_bounds, results)
+        results = filter(self.passable, results)
+        return results
+
+class GridWithWeights(SquareGrid):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+        self.weights = {}
+
+    def cost(self, from_node, to_node):
+        return self.weights.get(to_node, 1)
+
+diagram4 = GridWithWeights(10, 10)
+diagram4.walls = [(1, 7), (1, 8), (2, 7), (2, 8), (3, 7), (3, 8)]
+diagram4.weights = {loc: 5 for loc in [(3, 4), (3, 5), (4, 1), (4, 2),
+                                       (4, 3), (4, 4), (4, 5), (4, 6),
+                                       (4, 7), (4, 8), (5, 1), (5, 2),
+                                       (5, 3), (5, 4), (5, 5), (5, 6),
+                                       (5, 7), (5, 8), (6, 2), (6, 3),
+                                       (6, 4), (6, 5), (6, 6), (6, 7),
+                                       (7, 3), (7, 4), (7, 5)]}
+
+import heapq
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+def dijkstra_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
         if current == goal:
-            path = []
-            while current.parent:
-                path.append(current)
-                current = current.parent
-            path.append(current)
-            return path[::-1]
-        #Remove the item from the open set
-        openset.remove(current)
-        #Add it to the closed set
-        closedset.add(current)
-        #Loop through the node's children/siblings
-        for node in children(current,grid):
-            #If it is already in the closed set, skip it
-            if node in closedset:
-                continue
-            #Otherwise if it is already in the open set
-            if node in openset:
-                #Check if we beat the G score
-                new_g = current.G + current.move_cost(node)
-                if node.G > new_g:
-                    #If so, update the node to have a new parent
-                    node.G = new_g
-                    node.parent = current
-            else:
-                #If it isn't in the open set, calculate the G and H score for the node
-                node.G = current.G + current.move_cost(node)
-                node.H = manhattan(node, goal)
-                #Set the parent to our current item
-                node.parent = current
-                #Add it to the set
-                openset.add(node)
-    #Throw an exception if there is no path
-    raise ValueError('No Path Found')
-def next_move(pacman,food,grid):
-    #Convert all the points to instances of Node
-    for x in xrange(len(grid)):
-        for y in xrange(len(grid[x])):
-            grid[x][y] = Node(grid[x][y],(x,y))
-    #Get the path
-    path = aStar(grid[pacman[0]][pacman[1]],grid[food[0]][food[1]],grid)
-    #Output the path
-    print len(path) - 1
-    for node in path:
-        x, y = node.point
-        print x, y
-pacman_x, pacman_y = [ int(i) for i in raw_input().strip().split() ]
-food_x, food_y = [ int(i) for i in raw_input().strip().split() ]
-x,y = [ int(i) for i in raw_input().strip().split() ]
+            break
 
-grid = []
-for i in xrange(0, x):
-    grid.append(list(raw_input().strip()))
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost
+                frontier.put(next, priority)
+                came_from[next] = current
 
-next_move((pacman_x, pacman_y),(food_x, food_y), grid)
+    return came_from, cost_so_far
+
+# thanks to @m1sp <Jaiden Mispy> for this simpler version of
+# reconstruct_path that doesn't have duplicate entries
+
+def reconstruct_path(came_from, start, goal):
+    current = goal
+    path = []
+    while current != start:
+        path.append(current)
+        current = came_from[current]
+    path.append(start) # optional
+    path.reverse() # optional
+    return path
+
+def heuristic(a, b):
+    (x1, y1) = a
+    (x2, y2) = b
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def a_star_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+
+    while not frontier.empty():
+        current = frontier.get()
+
+        if current == goal:
+            break
+
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph.cost(current, next)
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                priority = new_cost + heuristic(goal, next)
+                frontier.put(next, priority)
+                came_from[next] = current
+
+    return came_from, cost_so_far
